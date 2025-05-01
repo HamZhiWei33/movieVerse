930dbbee331a1c3f94786666b6f66a755ee8a09a
HamZhiWei33 <149857735+HamZhiWei33@users.noreply.github.com>
2025-05-02 02:15:07 +0800

    update footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,33 +12,48 @@ import ProfilePage from "./pages/ProfilePage";
 import Footer from "./components/Footer";
 import MovieDetailPage from "./pages/MovieDetailPage";
 import NewReleasedPage from "./pages/NewReleasedPage";
-import { useEffect } from "react";
-import { UserValidationProvider } from "./context/UserValidationProvider ";
+import { useEffect, useContext } from "react";
+import {
+  UserValidationProvider,
+  UserValidationContext,
+} from "../src/context/UserValidationProvider "; // Fixed space and added provider import
+import FooterGuest from "./components/FooterGuest";
 
-function App() {
+// Define FooterSelector first since it's used in App
+function FooterSelector() {
+  const { isValidateUser } = useContext(UserValidationContext);
+  return isValidateUser ? <Footer /> : <FooterGuest />;
+}
+
+function AppContent() {
+  const { isValidatedUser } = useContext(UserValidationContext);
   return (
     <>
-      <UserValidationProvider>
-        <ScrollToTopOnNavigate />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/forgot_password" element={<ForgotPasswordPage />} />
-          <Route path="/reset_password" element={<ResetPasswordPage />} />
-          <Route path="/directory" element={<DirectoryPage />} />
-          <Route path="/ranking" element={<RankingPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/movie/:movieTitle" element={<MovieDetailPage />} />
-          <Route path="/new-released" element={<NewReleasedPage />} />
-        </Routes>
-        <Footer />
-      </UserValidationProvider>
+      <ScrollToTopOnNavigate />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgot_password" element={<ForgotPasswordPage />} />
+        <Route path="/reset_password" element={<ResetPasswordPage />} />
+        <Route path="/directory" element={<DirectoryPage />} />
+        <Route path="/ranking" element={<RankingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/movie/:movieTitle" element={<MovieDetailPage />} />
+        <Route path="/new-released" element={<NewReleasedPage />} />
+      </Routes>
+      <FooterSelector />
     </>
   );
 }
-
+function App() {
+  return (
+    <UserValidationProvider>
+      <AppContent />
+    </UserValidationProvider>
+  );
+}
 function ScrollToTopOnNavigate() {
   const { pathname } = useLocation();
 
