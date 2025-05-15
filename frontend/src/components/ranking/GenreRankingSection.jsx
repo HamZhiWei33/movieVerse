2214b785035a5311d3a676425fde63648b787e8b
WeiJunHng <weijun.hng@gmail.com>
2025-05-15 20:21:59 +0800

    Slice ranking from 1 to 10

--- frontend/src/components/ranking/GenreRankingSection.jsx
+++ frontend/src/components/ranking/GenreRankingSection.jsx
@@ -95,7 +95,7 @@ const GenreRankingSection = ({ movies, allGenres, allReviews }) => {
     });
   console.log("Sorted movies:", sorted); // Debugging line
   const top1 = sorted[0];
-  const otherMovies = sorted.slice(1);
+  const otherMovies = sorted.slice(1,10);
 
   const formatDuration = (minutes) => {
     if (!minutes || isNaN(minutes)) return "N/A";
