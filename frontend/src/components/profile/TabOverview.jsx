ade567d56f5853bcdaf8dc0c346cc803d5029120
HamZhiWei33 <149857735+HamZhiWei33@users.noreply.github.com>
2025-06-08 23:56:02 +0800

    get the actual user id

--- frontend/src/components/profile/TabOverview.jsx
+++ frontend/src/components/profile/TabOverview.jsx
@@ -4,20 +4,23 @@ import ReviewGenreChart from "./ReviewGenreChart";
 import LikesChart from "./LikesChart";
 import RateChart from "./RateChart";
 import WatchlistChart from "./WatchlistChart";
+import { useAuthStore } from "../../store/useAuthStore";
 // import "../../styles/profile/tab.css";
 
 const TabOverview = () => {
+  const { authUser } = useAuthStore();
+  const userId = authUser?._id;
   return (
     <div className="overview-container">
       <div className="chart-grid">
         <div className="chart chart-1">
-          <LikesChart userId="U2" />
+          <LikesChart userId={userId} />
         </div>
         <div className="chart chart-2">
-          <RateChart userId="U2" />
+          <RateChart userId={userId} />
         </div>
         <div className="chart chart-3">
-          <WatchlistChart userId="U2" />
+          <WatchlistChart userId={userId} />
         </div>
       </div>
     </div>
