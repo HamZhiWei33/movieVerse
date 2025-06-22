99e32cc07ed94e004fd4002e318aab5a70c18180
WeiJunHng <weijun.hng@gmail.com>
2025-06-23 03:11:52 +0800

    Format and delete unused route

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,5 @@
-// routes/userRoutes.js
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getCurrentUser,
   getUserProfile,
@@ -13,27 +13,32 @@ import {
   updateFavouriteGenres,
   changeNewPassword,
 } from "../controllers/user.controller.js";
-import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getUserLikedGenres,
   getUserReviewGenres,
   getUserWatchlistGenres,
   checkWatchlistStatus
 } from "../controllers/user.controller.js";
+
 const router = express.Router();
+
 router.put("/change-password", protectRoute, changeNewPassword);
 
 router.get("/me", protectRoute, getCurrentUser);
-router.get("/watchlist", protectRoute, getUserWatchlist);
+
 router.get("/review", protectRoute, getUserReviews);
 router.post("/review", protectRoute, addReview);
+
+router.get("/watchlist", protectRoute, getUserWatchlist);
 router.post("/watchlist/:movieId", protectRoute, addToWatchlist);
 router.delete("/watchlist/:movieId", protectRoute, removeFromWatchlist);
 router.get('/watchlist/:movieId/status', protectRoute, checkWatchlistStatus);
+
 router.get("/:id", getUserProfile);
 router.put("/:id", updateUserProfile);
 router.put("/:id/favourite-genres", updateFavouriteGenres);
 router.delete("/:id", protectRoute, deleteAccount);
+
 router.get("/:id/liked-genres", protectRoute, getUserLikedGenres);
 router.get("/:id/review-genres", protectRoute, getUserReviewGenres);
 router.get("/:id/watchlist-genres", protectRoute, getUserWatchlistGenres);
