73a343fd48e9791790b59e17fed533b14182c68c
WeiJunHng <weijun.hng@gmail.com>
2025-06-22 18:04:26 +0800

    Remove unused line in ProfilePage

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,14 +1,14 @@
+import "../styles/profile.css";
 import Profile from "../components/profile/Profile";
 import Tab from "../components/profile/Tab";
-import "../styles/profile.css";
+
 const ProfilePage = () => {
   return (
     <main className="profile-page">
-      {/* <h1>Profile Page</h1>
-      <p>This is the profile page.</p> */}
       <Profile />
       <Tab />
     </main>
   );
 };
+
 export default ProfilePage;
